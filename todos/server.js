b5615c68048a7347f0084bf0ee9f9bbce12874a4
punitha86 <punith.job@gmail.com>
2020-03-11 11:29:51 -0700

    mongoosedb connected

--- todos/server.js
+++ todos/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const todosController = require('./controllers/todos.js');
+const mongoose = require('mongoose');
 
 
 app.use('/todos', todosController);
 
+
+mongoose.connect('mongodb://localhost:27017/todocrud',
+                  {useNewUrlParser:true});
+mongoose.connection.once ('open', ()=>{
+    console.log('connected to mongoose...');
+});
 
 app.listen(3000, () => {
   console.log('listening..');;
