8fbac180f79f8482c2bb9db4d02a33cb80b2edc5
punitha86 <punith.job@gmail.com>
2020-03-12 10:54:49 -0700

    post route working

--- todos/controllers/todos.js
+++ todos/controllers/todos.js
@@ -1,8 +1,14 @@
 const express= require('express');
 const router = express.Router();
-
+const Todos = require('../models/todos.js');
 router.get('/', (req,res) => {
   res.send('index');
 });
 
+router.post('/', (req, res) => {
+    Todos.create(req.body, (error, createdTodo) => {
+        res.json(createdTodo);
+    });
+});
+////curl -X POST -H "Content-Type: application/json" -d '{"description":"weee","complete":true}' http://localhost:3000/todos/
 module.exports = router;
