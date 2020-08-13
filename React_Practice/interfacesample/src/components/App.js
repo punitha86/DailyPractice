a6d33e05bce1d61a78c02e94b70f8e2a029e40ef
punitha86 <punith.job@gmail.com>
2020-08-12 22:13:40 -0700

    addpet form addition and deletion

--- React_Practice/interfacesample/src/components/App.js
+++ React_Practice/interfacesample/src/components/App.js
@@ -6,15 +6,27 @@ import AddAppointments from './AddAppointments';
 import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
 
+import {without} from 'lodash';
+
 class App extends Component {
   constructor(){
     super();
     this.state = {
       myName:'Puni',
       myAppointments:[],
+      formDisplay:false,
       lastIndex: 0
     };
+    this.deleteAppointment=this.deleteAppointment.bind(this);
   }
+
+deleteAppointment (apt){
+  let tempApts = this.state.myAppointments;
+  tempApts = without(tempApts,apt);
+
+  this.setState({myAppointments:tempApts});
+}
+
   componentDidMount() {
     fetch('./data.json')
       .then(response => response.json())
@@ -41,9 +53,12 @@ class App extends Component {
             <div className="col-md-12 bg-white">
               <div className="container">
 
-                <AddAppointments />
+                <AddAppointments
+                  formDisplay={this.state.formDisplay}
+                />
                 <SearchAppointments />
-                <ListAppointments appointments={this.state.myAppointments}/>
+                <ListAppointments appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment}/>
               </div>
             </div>
           </div>
